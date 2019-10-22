8795a2a3196951a69484f6369426ebaf99327315
Eli Rodriguez <eli.rodriguezperez95@gmail.com>
2019-10-22 22:17:14 +0200

    [feat] updated GAME layer on both niu and planck

--- keyboards/planck/keymaps/eliseven/keymap.c
+++ keyboards/planck/keymaps/eliseven/keymap.c
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  | Shift|
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Ctrl | GUI  | Alt  | NUM  | Lower|Space |Space |Raise*| RAlt | RGui | RCtrl| MEH  |
+     * | Ctrl | HYPR |  GUI |  Alt | Lower|Space |Space |Raise*| RAlt | RGui | RCtrl| MEH  |
      * `-----------------------------------------------------------------------------------'
      *
      * This layer is supposed to be used for gaming, you normally cant Ctrl on
@@ -107,10 +107,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *
      * */
     [_GAME] = LAYOUT_ortho_4x12(
-            KC_TAB  , KC_Q    , KC_W   , KC_E , KC_R  , KC_T    , KC_Y    , KC_U   , KC_I    , KC_O    , KC_P    , KC_BSPC,
-            KC_ESC  , KC_A    , KC_S   , KC_D , KC_F  , KC_G    , KC_H    , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_QUOT,
-            KC_LSFT , KC_Z    , KC_X   , KC_C , KC_V  , KC_B    , KC_N    , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT,
-            KC_RCTL , KC_LGUI , KC_LALT, NUM  , LOWER , KC_SPC  , KC_SPC  , RAISE  , KC_ALGR , KC_RGUI , KC_RCTL , KC_MEH
+            KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T    , KC_Y    , KC_U   , KC_I    , KC_O    , KC_P    , KC_BSPC,
+            KC_ESC  , KC_A    , KC_S    , KC_D    , KC_F  , KC_G    , KC_H    , KC_J   , KC_K    , KC_L    , KC_SCLN , KC_QUOT,
+            KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B    , KC_N    , KC_M   , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT,
+            KC_RCTL , KC_HYPR , KC_LGUI , KC_LALT , LOWER , KC_SPC  , KC_SPC  , RAISE  , KC_ALGR , KC_RGUI , KC_RCTL , KC_MEH
             ),
 
     /* Lower
